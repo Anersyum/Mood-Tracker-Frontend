6b639dc9653c2ac52ecdea8e6e03bf10c552cc95
Amor Osmic <amorosmic@gmx.com>
2020-07-09 16:02:23 +0200

    Created a simple ui for diary entries and for creating diary entries

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiaryComponent implements OnInit {
 
+  isCreatingNewDiaryEntry = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  createNewDiaryEntry() {
+
+    this.isCreatingNewDiaryEntry = true;
+  }
+
+  cancelDiaryEntryCreation() {
+
+    this.isCreatingNewDiaryEntry = false;
+  }
+
+  createDiaryEntry() {
+    // create entry
+  }
 }
