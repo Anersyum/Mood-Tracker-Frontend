479dce8fe98dc83b02255192ef57b51e0b477e95
Amor Osmic <amorosmic@gmx.com>
2020-07-24 15:49:45 +0200

    Created functions for DM and add friend. They need functionallity. Created container to show fullname (username for now) under profile image.

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 import { UserSearchService } from '../_services/user-search.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-search',
@@ -10,7 +11,10 @@ import { Router } from '@angular/router';
 export class UserSearchComponent implements OnInit, DoCheck {
 
   users: any;
-  constructor(private userSearchService: UserSearchService, private router: Router) { }
+  showProfileInfo = false;
+  openedUser: any;
+
+  constructor(private userSearchService: UserSearchService, private router: Router, private http: HttpClient) { }
 
   ngDoCheck(): void {
 
@@ -29,16 +33,50 @@ export class UserSearchComponent implements OnInit, DoCheck {
 
       this.userSearchService.setUsername(username);
       this.userSearchService.searchUsers().subscribe((response: any) => {
-  
+
         console.log(response);
         this.userSearchService.setUsersResult(response);
         this.userSearchService.startSearch();
         this.users = this.userSearchService.getSearchedUsers();
       }, error => {
-  
+
         console.error(error);
       });
     }
+  }
+
+  getProfileInfo(id: number) {
+
+    this.http.get('http://localhost:5200/api/users/get/user/' + id, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    }).subscribe((response: any) => {
+
+      console.log(response);
+      this.openedUser = response;
+      this.showProfileInfo = true;
+    }, error => {
+
+      console.error(error);
+    });
+  }
+
+  addFriend(friendId: number) {
+
+    if (confirm('Want to add ' + friendId + ' to your friends list?')) {
+
+      alert('Added');
+    }
+    else {
+
+      alert('Canceled');
+    }
+  }
+
+  openDMBox() {
+
+    alert('Create a DM box');
   }
 
 }
